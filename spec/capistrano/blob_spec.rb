b379bd16e137bdeb73eec33531cc1d7a84e4b80a
William Johnston <will@johnstonclan.net>
2016-05-20 19:56:28 -0500

    Make useless tests pass.

--- spec/capistrano/blob_spec.rb
+++ spec/capistrano/blob_spec.rb
@@ -1,11 +1,7 @@
 require 'spec_helper'
 
-describe Capistrano::Blob do
+RSpec.describe Capistrano::Blob do
   it 'has a version number' do
     expect(Capistrano::Blob::VERSION).not_to be nil
-  end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
   end
 end
